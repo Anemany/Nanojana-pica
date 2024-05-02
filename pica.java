7fba59796d4af4e17ac4d1843d764536dd6c3788
Anemany <marija.nanojana@gmail.com>
2024-05-02 08:40:35 +0300

    jauns class pica

--- pica.java
+++ pica.java
@@ -2,7 +2,50 @@ import javax.swing.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+class Pica {
+    private String izmers;
+    private String toppings;
+    private String merce;
+    private boolean piegade;
 
+    private static final double MAZA_CENA = 8.99;
+    private static final double VIDĒJA_CENA = 10.99;
+    private static final double LIELA_CENA = 12.99;
+
+    public Pica(String izmers, String toppings, String merce, boolean piegade) {
+        this.izmers = izmers;
+        this.toppings = toppings;
+        this.merce = merce;
+        this.piegade = piegade;
+    }
+
+    public double aprekinatCenu() {
+        double kopsumma = 0;
+
+        switch (izmers) {
+            case "Mazā":
+                kopsumma += MAZA_CENA;
+                break;
+            case "Vidējā":
+                kopsumma += VIDĒJA_CENA;
+                break;
+            case "Liela":
+                kopsumma += LIELA_CENA;
+                break;
+        }
+
+        return kopsumma;
+    }
+
+    @Override
+    public String toString() {
+        String piegadesVeids = piegade ? "Piegāde" : "Ja";
+        return "Picas izmērs: " + izmers +
+                "\nToppings: " + toppings +
+                "\nMērce: " + merce +
+                "\nPiegāde: " + piegadesVeids;
+    }
+}
 public class Pica {
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog(null, "Ievadiet savu vārdu:");
@@ -23,5 +66,7 @@ public class Pica {
                 "Vai vēlaties piegādi?", "Piegāde",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         boolean piegade = piegadesIzvele == JOptionPane.YES_OPTION;
+
+       
     }
 }
