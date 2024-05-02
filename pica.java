8eeae0a9ba5ce01355160cd4f348009b464dad74
Anemany <marija.nanojana@gmail.com>
2024-05-02 08:10:08 +0300

    ievadit vardu, adrese un numurs

---
+++ pica.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+public class Pica {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog(null, "Vards:");
+        String address = JOptionPane.showInputDialog(null, "Adrese:");
+        String phone = JOptionPane.showInputDialog(null, "Numurs:");
+
+    }}
