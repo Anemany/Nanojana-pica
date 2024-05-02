a60c57d2a019900573e0893c9bdca2452425265e
Anemany <marija.nanojana@gmail.com>
2024-05-02 08:35:22 +0300

    order ir glabats visa informacija par klientu un pica ir izmainas lai klients var izvelet toppingu

--- pica.java
+++ pica.java
@@ -1,10 +1,28 @@
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Pica {
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog(null, "Vards:");
-        String address = JOptionPane.showInputDialog(null, "Adrese:");
-        String phone = JOptionPane.showInputDialog(null, "Numurs:");
+        String name = JOptionPane.showInputDialog(null, "Ievadiet savu vārdu:");
+        String address = JOptionPane.showInputDialog(null, "Ievadiet savu adresi:");
+        String phoneNumber = JOptionPane.showInputDialog(null, "Ievadiet savu telefona numuru:");
+        String[] izmēri = {"Mazā", "Vidējā", "Liela"};
+        String izmers = (String) JOptionPane.showInputDialog(null, "Izvēlieties pīcas izmēru:",
+                "Pīcas izmērs", JOptionPane.PLAIN_MESSAGE, null, izmēri, izmēri[0]);
 
-    }}
+        String[] toppings = {"Siers", "Pepperoni", "Sēnes", "Sīpoli"};
+        String topping = (String) JOptionPane.showInputDialog(null, "Izvēlieties pīcas toppings:",
+                "Pīcas toppings", JOptionPane.PLAIN_MESSAGE, null, toppings, toppings[0]);
+
+        String[] mērces = {"Marināde", "Barbecue", "Ķiploku"};
+        String merce = (String) JOptionPane.showInputDialog(null, "Izvēlieties pīcas mērci:",
+                "Pīcas mērce", JOptionPane.PLAIN_MESSAGE, null, mērces, mērces[0]);
+
+        int piegadesIzvele = JOptionPane.showConfirmDialog(null,
+                "Vai vēlaties piegādi?", "Piegāde",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        boolean piegade = piegadesIzvele == JOptionPane.YES_OPTION;
+    }
+}
